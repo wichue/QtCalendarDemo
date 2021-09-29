447c6593b925336f205cadf3b90f31ee7070e6fb
wichue <13770941037@139.com>
2021-09-29 14:41:44 +0800

    第一次提交
    UI界面新建一个QLineEdit，提升为QLineEditCalendar，点击即可自动弹出日历

---
+++ QtCalendarDemo/calendar/qlineeditcalendar.cpp
@@ -0,0 +1,22 @@
+﻿#include "qlineeditcalendar.h"
+#include <QEvent>
+#include <QDebug>
+
+QLineEditCalendar::QLineEditCalendar(QWidget *parent) : QLineEdit(parent)
+{
+    pLunarCalendarWidget = new LunarCalendarWidget();
+    connect(pLunarCalendarWidget,&LunarCalendarWidget::signalCurrData,this,&QLineEditCalendar::slotCurrData);
+}
+
+void QLineEditCalendar::slotCurrData(QString data)
+{
+    this->setText(data);
+}
+
+void QLineEditCalendar::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        pLunarCalendarWidget->show();
+    }
+}
---
+++ QtCalendarDemo/calendar/qlineeditcalendar.h
@@ -0,0 +1,33 @@
+﻿/*!
+ *  @brief     自定义QLineEdit，点击弹出日历，可选择日期上午下午
+ *  @details
+ *  @author    褚伟(114639950@qq.com)
+ *  @version
+ *  @date      2021.7.22
+ *  @warning
+ *  @copyright NanJing HS
+ *  @note
+ */
+
+#ifndef QLINEEDITCALENDAR_H
+#define QLINEEDITCALENDAR_H
+
+#include <QLineEdit>
+#include <QWidget>
+#include <QMouseEvent>
+#include "lunarcalendarwidget.h"
+
+class QLineEditCalendar : public QLineEdit
+{
+    Q_OBJECT
+public:
+    explicit QLineEditCalendar(QWidget *parent = nullptr);
+private:
+    LunarCalendarWidget *pLunarCalendarWidget;
+public slots:
+    void slotCurrData(QString data);
+protected:
+    virtual void mousePressEvent(QMouseEvent *event);
+};
+
+#endif // QLINEEDITCALENDAR_H
